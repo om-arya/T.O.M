cf30314a9d942d1c7d99a257a3e4056b892976aa
om-arya <om.arya0577@gmail.com>
2024-05-22 11:48:32 -0400

    Set up front end files with Django

--- djangofiles/settings.py
+++ djangofiles/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = 'django-insecure-hmxo1oxp+03*s96+n2vcdouice^doa4in2+$47--lm&_n#&)_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # SECURITY WARNING: '*' allows all. CHANGE BEFORE DEPLOYMENT!!!
 
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TOM.urls'
+ROOT_URLCONF = 'djangofiles.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TOM.wsgi.application'
+WSGI_APPLICATION = 'djangofiles.wsgi.application'
 
 
 # Database
@@ -115,8 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [Path(BASE_DIR, 'static')]
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
