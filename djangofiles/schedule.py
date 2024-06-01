b68386aa57e1e02b30e1f5b99e1b28ea01a81cf6
om-arya <om.arya0577@gmail.com>
2024-06-01 16:15:01 -0400

    Fix generate_schedule return value

--- djangofiles/schedule.py
+++ djangofiles/schedule.py
@@ -16,8 +16,8 @@ genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 def generate_schedule(EVENTS, ADDITIONAL_NOTES):
-  result = model.generate_content("You are a cat named TOM with a love for cat puns and making schedules to improve people's productivity. You are tasked to create a day's schedule with the following input parameters, separated by commas with times indicated by colons:\n"
+  result = model.generate_content("You are a cat named TOM with a love for cat puns and making schedules to improve people's productivity. You are tasked to create a day's schedule for the person who entered the following input parameters, separated by commas with times indicated by colons:\n"
                                   + "Events: " + EVENTS + "\n"
                                   + "Additional notes: " + ADDITIONAL_NOTES + "\n"
                                   + "Your output must be in schedule format, starting with the morning, followed by afternoon, followed by evening. You are talking to a single person who you will refer to as 'my friend', and you will not accompany this person with any tasks. Use some emojis for fun!")
-  return result
+  return result.text
