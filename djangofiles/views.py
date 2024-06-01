19684cf0023513ed2a2128c92f6f10a9189433ee
om-arya <om.arya0577@gmail.com>
2024-06-01 13:14:18 -0400

    Add HTTP response to views.py

--- djangofiles/views.py
+++ djangofiles/views.py
@@ -20,5 +20,7 @@ def submit_form(request):
     if request.method == 'GET':
         EVENTS = request.GET.get('events')
         ADDITIONAL_NOTES = request.GET.get('additionalNotes')
+        # Send an HTTP response for now; we want to send the variables to schedule.py
+        return HttpResponse(f"Events: {EVENTS}, Additional Notes: {ADDITIONAL_NOTES}")
     else:
         return HttpResponse("Only GET method is allowed.")
