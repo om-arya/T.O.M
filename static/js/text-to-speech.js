3d0c1b5459b774635374e11ee936b3dd77bec5b5
om-arya <om.arya0577@gmail.com>
2024-06-21 18:52:39 -0400

    Default to male voice for TTS

--- static/js/text-to-speech.js
+++ static/js/text-to-speech.js
@@ -1,9 +1,36 @@
 const synth = window.speechSynthesis;
-function textToSpeech(message) {
+const maleVoices = [
+    "Microsoft David Desktop - English (United States)",
+    "Alex",
+    "Daniel",
+    "Sin-Ji",
+    "Maged",
+];
+
+function loadVoices() {
+    return new Promise((resolve) => {
+        let voices = synth.getVoices();
+        if (voices.length !== 0) {
+            resolve(voices);
+        } else {
+            synth.onvoiceschanged = () => {
+                voices = synth.getVoices();
+                resolve(voices);
+            };
+        }
+    });
+}
+
+const voices = await loadVoices();
+const voice = voices.find(({ name }) => maleVoices.includes(name));
+
+function textToSpeech(message, rate=1, pitch=.8) {
     const speakThis = new SpeechSynthesisUtterance(message);
     speakThis.lang = 'en';
-    speakThis.pitch = .8;
-    speakThis.rate = 1;
+    speakThis.rate = rate;
+    speakThis.pitch = pitch;
+    speakThis.voice = voice;
+
     synth.speak(speakThis);
 }
 
