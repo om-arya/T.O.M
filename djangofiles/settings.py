cfaece1c431a15f2149e59e39aca027414a2dbf8
om-arya <om.arya0577@gmail.com>
2024-06-21 19:58:23 -0400

    Edit deployment config

--- djangofiles/settings.py
+++ djangofiles/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'djangofiles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
