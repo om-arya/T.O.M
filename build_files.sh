a80f0bf3f96a71a382409d3381f4dd3016047427
om-arya <om.arya0577@gmail.com>
2024-06-21 20:07:05 -0400

    Edit deployment config

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-pip3 install -r requirements.txt
+# Create and activate a virtual environment
+python3.9 -m venv venv
+source .venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
 python3.9 manage.py collectstatic
