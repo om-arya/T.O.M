20b939e209e7ca112432bd2f7940b81c022e8fe9
heavens-potato <sswu2005@gmail.com>
2024-07-22 20:55:04 +0800

    Added additional text to About TOM
    The two coding tom pictures are place holders

---
+++ static/js/about-tom.js
@@ -0,0 +1,25 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const title = document.querySelector(".big-title");
+//     const subtitle = document.querySelector(".subtitle")
+
+//     console.log(title.textContent);
+//     console.log(subtitle.textContent);
+
+//     typingEffect(title, title.textContent);
+//     typingEffect(subtitle, subtitle.textContent);
+// });
+
+// async function typingEffect(element, text) {
+//     setTimeout(() => {
+//         element.textContent = "";
+//     }, 0);
+
+//     let ch = 0;
+//     typeInterval = setInterval(() => {
+//         element.textContent += text[ch];
+//         ch += 1;
+//         if (ch == text.length) {
+//             clearInterval(typeInterval);
+//         }
+//     }, 50);
+// }
