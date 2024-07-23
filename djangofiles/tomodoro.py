c4f7f768eda6efbd7c666d93d63c5f7bb9397034
heavens-potato <sswu2005@gmail.com>
2024-07-23 18:31:59 +0800

    Finished adding About TOM text content (and attempted to fix Gemini prompt in py but it's I think Gemini is ignoring me)

--- djangofiles/tomodoro.py
+++ djangofiles/tomodoro.py
@@ -6,8 +6,8 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 def generate_colors(mood: str):
   prompt = (
-    "Generate 4 complimentary color hex codes for a background, a different color for the background, buttons, and the text on the buttons "
-    "The text color cannot be black."
+    "Generate 4 complimentary but contrasting but complimentary color hex codes for page colors, with two different colors for the background, buttons, and the text on the buttons. "
+    "The text cannot be a similar color to the button color, and the buttons cannot be a similar color to the background. The text on the buttons must not be black, and the two background colors must be different."
     "(Your response must be formatted exactly as '#xyzabc,#xyzabc,#xyzabc,#xyzabc') "
     f"based on the following 'mood': { mood }"
   ) 
