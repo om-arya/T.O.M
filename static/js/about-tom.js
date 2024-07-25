c879b307637ab603812ea63c550ee716032d6bc8
heavens-potato <sswu2005@gmail.com>
2024-07-25 17:56:26 +0800

    Transition and layout adjustments to About TOM, plus added exit transition

--- static/js/about-tom.js
+++ static/js/about-tom.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     clearInterval(typeInterval);
                     resolve();
                 }
-            }, 35);
+            }, 40);
         });
     }
 
@@ -55,4 +55,32 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     triggerTypingEffect();
+
+
+    /**
+    * EXIT ANIMATION CODE: We first select all the a href tags on the page- these are only used to lead away from the page itself
+    * so page functionality isn't impacted. We then add an event listener for these links, and if any of them is clicked, we call 
+    * preventDefault() to prevent them from directly sending the user to the next page. We then put all the elements we want to have disappear 
+    * (in this case everything but the menu bar and footer) on the exit-animation classlist, which makes them fade into opacity 0. After that, 
+    * the link directs the user to the page containing the link. If the responsive version of the nav bar is visible, the transitions 
+    * do not play to prevent the page contents from transitioning away before the user has gone to a new page. 
+     */
+    const allLinks = document.querySelectorAll('a');
+
+    allLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+    
+            //stops link from just sending the user to the next page.
+            event.preventDefault();
+
+            //add everything below the menu bar and above the footer to the exit animation class list so the animation plays.
+            const allElements = document.getElementById('all-content');
+            allElements.classList.add('exit');
+  
+            //delay after animation and send the user to the link
+            setTimeout(() => {
+                window.location.href = link.href;
+            }, 500);
+        });
+    });
 });
