bcaadcf1fb0c4347f80a01c07f471b93a4e4f864
Claire <claire13985@gmail.com>
2024-07-25 00:59:43 -0400

    Added typing effect and delay to transitions

--- static/js/about-tom.js
+++ static/js/about-tom.js
@@ -1,23 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            }
+
+    function typingEffect(element, text) {
+        return new Promise((resolve) => {
+            element.textContent = ""; 
+            element.classList.add('visible');
+            let ch = 0;
+            const typeInterval = setInterval(() => {
+                element.textContent += text[ch];
+                ch += 1;
+                if (ch === text.length) {
+                    clearInterval(typeInterval);
+                    resolve();
+                }
+            }, 35);
         });
-    });
+    }
 
-    const observerRight = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show-tom');
-            }
+    const triggerTypingEffect = async () => {
+        const bigTitle = document.querySelector(".big-title");
+        const subtitle = document.querySelector(".subtitle");
+
+        if (bigTitle) {
+            await typingEffect(bigTitle, bigTitle.textContent);
+        }
+        if (subtitle) {
+            await typingEffect(subtitle, subtitle.textContent);
+        }
+        activateObservers();
+    };
+
+    const activateObservers = () => {
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                }
+            });
         });
-    });
 
-    const hiddenElements = document.querySelectorAll(".hidden");
-    hiddenElements.forEach((el) => observer.observe(el));
+        const observerRight = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show-tom');
+                }
+            });
+        });
 
-    const hiddenElementsRight = document.querySelectorAll(".hidden-tom");
-    hiddenElementsRight.forEach((el) => observerRight.observe(el));
+        const hiddenElements = document.querySelectorAll(".hidden");
+        hiddenElements.forEach((el) => observer.observe(el));
+
+        const hiddenElementsRight = document.querySelectorAll(".hidden-tom");
+        hiddenElementsRight.forEach((el) => observerRight.observe(el));
+    };
+
+    triggerTypingEffect();
 });
