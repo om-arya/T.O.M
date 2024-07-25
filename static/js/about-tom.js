04bc50b867c9d0bef19105ae2f9935a6648f4689
Claire <claire13985@gmail.com>
2024-07-25 00:41:38 -0400

    Added transitions to About TOM page

--- static/js/about-tom.js
+++ static/js/about-tom.js
@@ -1,27 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    });
 
+    const observerRight = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show-tom');
+            }
+        });
+    });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const title = document.querySelector(".big-title");
-//     const subtitle = document.querySelector(".subtitle")
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
 
-//     console.log(title.textContent);
-//     console.log(subtitle.textContent);
-
-//     typingEffect(title, title.textContent);
-//     typingEffect(subtitle, subtitle.textContent);
-// });
-
-// async function typingEffect(element, text) {
-//     setTimeout(() => {
-//         element.textContent = "";
-//     }, 0);
-
-//     let ch = 0;
-//     typeInterval = setInterval(() => {
-//         element.textContent += text[ch];
-//         ch += 1;
-//         if (ch == text.length) {
-//             clearInterval(typeInterval);
-//         }
-//     }, 50);
-// }
+    const hiddenElementsRight = document.querySelectorAll(".hidden-tom");
+    hiddenElementsRight.forEach((el) => observerRight.observe(el));
+});
