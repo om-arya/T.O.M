4328a57419d4f6780a84bafa25e6c6153e49d589
heavens-potato <sswu2005@gmail.com>
2024-07-29 15:56:43 +0800

    About Us content update and minor Gemini prompt updates

--- djangofiles/tomodoro.py
+++ djangofiles/tomodoro.py
@@ -7,7 +7,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 def generate_colors(mood: str):
   prompt = (
     "Generate 4 complimentary but contrasting color hex codes for page colors, with two different pastel colors for the background, one deep and saturated color for buttons, and a whiteish color for text on the buttons. "
-    "The buttons must stand out on the background, and the text must stand out on the button.  The text cannot be black.  The two background colors must be two different colors."
+    "The buttons must stand out on the background, and the text must stand out on the button.  The text cannot be black.  The two background colors must be two different colors, and they must not be darker than gray."
     "(Your response must be formatted exactly as '#xyzabc,#xyzabc,#xyzabc,#xyzabc' in the order of 'background, background, buttons, text')  "
     f"based on the following 'theme': { mood }"
   ) 
