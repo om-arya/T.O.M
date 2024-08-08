2fca0f54911f4147494af4a3cbe42b98bbeb9f2b
Justin Glazer <jglazer365@gmail.com>
2024-08-07 23:30:58 -0400

    readded red screen for timer end

--- static/js/timer.js
+++ static/js/timer.js
@@ -55,6 +55,7 @@ function decrementSecond() {
     if (currTime === "00:00") {
         // Change screen color and play alarm until either
         // the restart button or the start button is clicked.
+        body.style["background"] = "linear-gradient(white, red)";
         playAlarm();
 
         startButton.addEventListener('click', stopTimer);
