17828d8f2a33cf6bbff0b99aa05b39d343f45d2f
om-arya <om.arya0577@gmail.com>
2024-08-11 11:28:27 -0400

    Re-add exit animation code

--- static/js/animations.js
+++ static/js/animations.js
@@ -11,36 +11,6 @@ function addResponsiveClassToNavbar() {
     });
 }
 
-/**
- * We first select all the a href tags on the page- these are only used to lead away from the page itself
- * so page functionality isn't impacted. We then add an event listener for these links, and if any of them is clicked, we call 
- * preventDefault() to prevent them from directly sending the user to the next page. We then put all the elements we want to have disappear 
- * (in this case everything but the menu bar and footer) on the exit-animation classlist, which makes them fade into opacity 0. After that, 
- * the link directs the user to the page containing the link.
- */
-function addExitAnimation() {
-    const allLinks = document.querySelectorAll('.nav-items a');
-
-    allLinks.forEach(link => {
-        if(!link.classList.contains('small-menu-link')) {
-            link.addEventListener('click', (event) => {
-        
-                //stops link from just sending the user to the next page.
-                event.preventDefault();
-        
-                //add everything below the menu bar and above the footer to the exit animation class list so the animation plays.
-                const formElements = document.getElementById('timer-div');
-                formElements.classList.add('exit');
-        
-                //delay after animation and send the user to the link
-                setTimeout(() => {
-                    window.location.href = link.href;
-                }, 500);
-            });
-        }
-    });
-}
-
 function addMenuToggle() {
     const menuToggle = document.querySelector('.menu-toggle');
     const menuContent = document.querySelector('.menu-content');
@@ -50,4 +20,4 @@ function addMenuToggle() {
     });
 }
 
-export { addResponsiveClassToNavbar, addExitAnimation, addMenuToggle }
+export { addResponsiveClassToNavbar, addMenuToggle }
--- static/js/tomodoro.js
+++ static/js/tomodoro.js
@@ -1,5 +1,5 @@
 import { setStartTime, runTimer, stopTimer } from "./timer.js";
-import { addResponsiveClassToNavbar, addExitAnimation, addMenuToggle } from "./animations.js";
+import { addResponsiveClassToNavbar, addMenuToggle } from "./animations.js";
 
 const timer = document.querySelector('.timer');
 const startButton = document.querySelector('#start-timer');
@@ -335,6 +335,36 @@ async function typingEffect(element, text) {
     }, 200);
 }
 
+/**
+ * EXIT ANIMATION CODE: We first select all the a href tags on the page- these are only used to lead away from the page itself
+ * so page functionality isn't impacted. We then add an event listener for these links, and if any of them is clicked, we call 
+ * preventDefault() to prevent them from directly sending the user to the next page. We then put all the elements we want to have disappear 
+ * (in this case everything but the menu bar and footer) on the exit-animation classlist, which makes them fade into opacity 0. After that, 
+ * the link directs the user to the page containing the link.
+ */
+const allLinks = document.querySelectorAll('.nav-items a');
+
+allLinks.forEach(link => {
+    //filter out the a href that is the menu icon when the page is smaller to avoid having elements transition before the user has selected another page
+    if(!link.classList.contains('small-menu-link')) {
+        link.addEventListener('click', (event) => {
+
+            //stops link from just sending the user to the next page.
+            event.preventDefault();
+
+            //add everything below the menu bar and above the footer to the exit animation class list so the animation plays.
+            const formElements = document.getElementById('scheduleForm');
+            formElements.classList.add('exit');
+            const result = document.getElementById('result');
+            result.classList.add('exit');
+
+            //delay after animation and send the user to the link
+            setTimeout(() => {
+                window.location.href = link.href;
+            }, 500);
+        });
+    }
+});
+
 addResponsiveClassToNavbar();
-addExitAnimation();
 addMenuToggle();
